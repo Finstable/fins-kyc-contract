db7cabb2b122b2c3c502e1379fc2cc80bdb3f772
Warun S <warunsinx@gmail.com>
2023-06-29 17:09:16 +0700

    implemented FINSKYC Interface

---
+++ contracts/interfaces/IFINSKYC.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+import "./IFINSProjectRegistry.sol";
+
+interface IFINSKYC {
+    struct KYCWallet {
+        string uid;
+        uint256 kycLevel;
+        uint256 updatedAt;
+    }
+
+    function projectRegistry() external view returns (IFINSProjectRegistry);
+
+    function kycWallets(address) external view returns (KYCWallet memory);
+
+    function kycAdmin() external view returns (address);
+
+    function setKYCLevel(
+        address walletAddress,
+        string memory uid,
+        uint256 kycLevel
+    ) external;
+
+    function updateKYCLevel(address walletAddress, uint256 kycLevel) external;
+
+    function setKYCAdmin(address _kycAdmin) external;
+
+    function setFINSProjectRegistry(address _projectRegistry) external;
+
+    function getKYCLevel(address walletAddress) external view returns (uint256);
+}
